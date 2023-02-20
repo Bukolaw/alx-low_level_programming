092a4a3391f136048be44919236cf55cd39c4b62
Bukolaw <officialbukolaa@gmail.com>
2023-02-20 15:46:26 -0800

    lob tan

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Hexadecimal, 16.Betty plz :)
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(i < 10 ? i + '0' : i + 'a' - 10);
+	}
+	putchar('\n');
+
+	return (0);
+}
+

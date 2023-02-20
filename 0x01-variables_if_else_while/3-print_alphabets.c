10ae32f99b9fc47757773812bcaa63918cbab639
Bukolaw <officialbukolaa@gmail.com>
2023-02-20 14:26:26 -0800

    owanbe

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet Game - CAPS & lowers
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(1);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(1);
+	}
+	putchar('\n');
+	return (0);
+}

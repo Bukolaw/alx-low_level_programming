d3abb27941c9d088c2b74667b8d99184f5b1a619
Bukolaw <officialbukolaa@gmail.com>
2023-02-20 17:58:42 -0800

    cockerel

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,13 +15,14 @@ int main(void)
 		{
 			putchar((i / 10) + '0'); /* print first digit of i */
 			putchar((i % 10) + '0'); /* print second digit of i */
-			putchar(' ')
+			putchar(' ');
 			putchar((j / 10) + '0'); /* print first digit of j */
-			putchar((j % 10) + '0"); /* print second digit of j */
-			if (i != 98 || j != 99) / * check if it's the last
-				combination * /
+			putchar((j % 10) + '0'); /* print second digit of j */
+			if (i != 98 || j != 99) /* check if it's the last
+						   * combination
+				*/
 			{
-				putchar(',');
+				putchar(' , ');
 				putchar(' ');
 			}
 		}

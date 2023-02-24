8d3fac511123191b672f4b254e306151fc4b375b
Bukolaw <officialbukolaa@gmail.com>
2023-02-23 16:17:31 -0800

    do well

--- 0-isupper.c
+++ 0-isupper.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * _isupper - afunction that checks for uppercase character
+ * _isupper - a function that checks for uppercase character
+ * int: number withour fractional component
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
+int _isupper(void);
+
+/**
+ * main - check the code
+ * Description: distinct capital
+ * Return: Always 0.
+ */
+
+	int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+
 int _isupper(int c)
 {
+/**
+ * _isupper - function that check for uppercase
+ */
+
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
 		return (0);
+}
+
 }
